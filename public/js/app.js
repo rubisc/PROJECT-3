84e74aaf9d814fdc196afa8fc0392e73a16488de
Rubi Cabral <rubisc@gmail.com>
2017-05-04 12:27:44 -0700

    delete and favorite button commented back in

--- public/js/app.js
+++ public/js/app.js
@@ -6,17 +6,17 @@ $(document).ready(function(){
   //   User.favorites.push(this._id)
   // ){})
   //
-  // $('.userDelBtn').on('click', function(){
-  //   var user = $(this)
-  //   var userDiv = user.parent()
-  //   var id = user.attr('id')
-  //   $.ajax({
-  //     method: "DELETE",
-  //     url: `/users/${id}`
-  //   }).done(function(){
-  //     userDiv.remove()
-  //   })
-  // })
+  $('.userDelBtn').on('click', function(){
+    var user = $(this)
+    var userDiv = user.parent()
+    var id = user.attr('id')
+    $.ajax({
+      method: "DELETE",
+      url: `/users/${id}`
+    }).done(function(){
+      userDiv.remove()
+    })
+  })
 
 
 
